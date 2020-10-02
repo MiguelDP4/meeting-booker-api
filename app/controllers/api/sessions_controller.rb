c34876bf318e8906b9d439aa66bdaa2b0f72b520
Miguel <migueldp4@gmail.com>
2020-10-02 09:32:15 -0600

    Create user controller, index render

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  class SessionsController < ApplicationController
+    def create
+      user = User
+              .find_by(name: params["user"]["name"])
+              .try(:authenticate, params["user"]["password"])
+
+      if user
+        session[:user_id] = user.id
+        render json: {
+          status: :created,
+          logged_in: true,
+          user: user
+        }
+      else
+        render json: { status: 'ERROR', message: 'Could not start session. Incorrect user or password' }, status: :unprocessable_entry
+      end
+    end
+  end
+end
