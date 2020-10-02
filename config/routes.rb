7c453e5b001ac345ab08940ab5ec7a2564bad386
Miguel <migueldp4@gmail.com>
2020-10-02 12:17:41 -0600

    user can get, post, delete and patch requests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #resources :sessions, only: [:create]
   namespace 'api' do
-    resources :users
+    get 'users', to: 'users#index'
+    get 'users/:name', to: 'users#show'
+    post 'users', to: 'users#create'
+    delete 'users/:name', to: 'users#destroy'
+    patch 'users/:name', to: 'users#update'
   end
 end
