ed2829c96b5bed3d0408ec8e98f9e1e25bdc886d
Miguel <migueldp4@gmail.com>
2020-10-06 14:46:16 -0600

    Adapt to work on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'bookings#index'
   namespace 'api' do
     post 'login',                     to: 'sessions#log_in'
     delete 'logout',                  to: 'sessions#logout'
